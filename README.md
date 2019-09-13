329b457eb4564e4333c8da77e092c393f04f4f91
geiti94 <geiti94@me.com>
2019-09-13 12:33:28 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Recovery Device Tree for the Samsung Galaxy A50
+
+## How-to compile it:
+
+To build:
+
+```sh
+. build/envsetup.sh
+lunch omni_a50dd-eng
+make recoveryimage
---
+++ omni_a50dd.mk
@@ -0,0 +1,31 @@
+#
+# Copyright 2018 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := a50dd
+
+
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a50dd
+PRODUCT_NAME := omni_a50dd
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := Galaxy A50
+PRODUCT_MANUFACTURER := Samsung
